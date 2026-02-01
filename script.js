6e700eec25df89ef7401560b243f917100d0cc1e
Radhika Irkal <radhikamirkal@gmail.com>
2026-02-01 13:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+let currentAudio = null;
+
+// Play sound when button clicked
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const sound = button.getAttribute("data-sound");
+
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio.play();
+  });
+});
+
+// Stop button logic
+document.querySelector(".stop").addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
